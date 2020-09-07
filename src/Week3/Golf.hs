db40284e7452dfaa3e4886d1bdb2726a73ad5e2f
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-07 08:12:16 +0530

    Trying exercise 3

--- src/Week3/Golf.hs
+++ src/Week3/Golf.hs
@@ -61,3 +61,40 @@ localMaxima (p:q:r:xs)
 localMaxima _ = []
 
 
+{-
+ Exercise 3 : Histogram
+
+ For this task, write a function
+   histogram :: [Integer] -> String
+ which takes as input a list of Integers between 0 and 9 (inclusive),
+ and outputs a vertical histogram showing how many of each number
+ were in the input list. You may assume that the input list does not
+ contain any numbers less than zero or greater than 9 (that is, it does
+ not matter what your function does if the input does contain such numbers).
+
+ Your output must exactly match the output shown in the examples below.
+
+ histogram [1,1,1,5] ==
+ *
+ *
+ * *
+ ==========
+ 0123456789
+ histogram [1,4,5,4,6,6,3,4,2,4,9] ==
+
+ *
+ *
+ * *
+ ****** *
+ ==========
+ 0123456789
+
+ Important note: If you type something like histogram [3,5] at the ghci prompt,
+ you should see something like this:
+  " * * \n==========\n0123456789\n"
+
+ This is a textual representation of the String output, including \n
+ escape sequences to indicate newline characters. To actually visualize
+ the histogram as in the examples above, use putStr, for example,
+ putStr (histogram [3,5]).
+-}
