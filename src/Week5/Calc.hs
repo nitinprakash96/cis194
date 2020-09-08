f0d495fe844528b238f77a0c3ecb3460f0c75f15
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-08 17:46:45 +0530

    Start exercise 6

--- src/Week5/Calc.hs
+++ src/Week5/Calc.hs
@@ -306,3 +306,50 @@ instance Expr SVM.Program where
 -- Just [PushI 3,PushI (-4),Mul,PushI 5,Add]
 compile :: String -> Maybe SVM.Program
 compile = parseExp lit add mul
+
+
+{-
+ Exercise 6:
+
+ Some users of your calculator have requested the ability to give names to
+ intermediate values and then reuse these stored values later.
+
+ To enable this, you first need to give arithmetic expressions the ability
+ to contain variables. Create a new type class HasVars a which contains a
+ single method var :: String -> a. Thus, types which are instances of HasVars
+ have some notion of named variables.
+
+ Start out by creating a new data type VarExprT which is the same as ExprT
+ but with an extra constructor for variables. Make VarExprT an instance of
+ both Expr and HasVars. You should now be able to write things like
+
+    *Calc> add (lit 3) (var "x") :: VarExprT
+
+ But we can’t stop there: we want to be able to interpret expressions containing
+ variables, given a suitable mapping from variables to values. For storing mappings
+ from variables to values, you should use the Data.Map module. Add
+
+   import qualified Data.Map as M
+
+ at the top of your file. The qualified import means that you must
+ prefix M. whenever you refer to things from Data.Map. This is standard practice,
+ since Data.Map exports quite a few functions with names that overlap with names
+ from the Prelude. Consult the Data.Map documentation to read about the operations
+ that are supported on Maps.
+
+ Implement the following instances:
+   instance HasVars (M.Map String Integer -> Maybe Integer)
+   instance Expr (M.Map String Integer -> Maybe Integer)
+
+ The first instance says that variables can be interpreted as functions from a
+ mapping of variables to Integer values to (possibly) Integer values. It should
+ work by looking up the variable in the mapping.
+
+ The second instance says that these same functions can be interpreted as expressions
+ (by passing along the mapping to subexpressions and combining results appropriately).
+
+ Note: to write these instances you will need to enable the FlexibleInstances
+ language extension by putting
+   {-# LANGUAGE FlexibleInstances #-}
+ as the first line in your file.
+-}
