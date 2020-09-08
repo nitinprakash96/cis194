ef96353fe0cf6c3dcdeedc70cb6420e2462db233
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-08 11:43:44 +0530

    Exercise 2 variations

--- src/Week5/Calc.hs
+++ src/Week5/Calc.hs
@@ -1,5 +1,8 @@
 module Week5.Calc where
 
+import Week5.ExprT
+import Week5.Parser
+import Data.Maybe
 
 {-
  PREFACE:
@@ -38,3 +41,53 @@ module Week5.Calc where
 
  For example, eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) == 20.
 -}
+eval :: ExprT -> Integer
+eval (Lit n)   = n
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
+
+
+{-
+ Exercise 2:
+
+ The UI department has internalized the focus group data and is ready to
+ synergize with you. They have developed the front-facing user-interface:
+ a parser that handles the textual representation of the selected language.
+ They have sent you the module Parser.hs, which exports parseExp, a parser
+ for arithmetic expressions. If you pass the constructors of ExprT to it as
+ arguments, it will convert Strings representing arithmetic expressions into
+ values of type ExprT.
+
+ For example:
+    *Calc> parseExp Lit Add Mul "(2+3)*4"
+    Just (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+
+    *Calc> parseExp Lit Add Mul "2+3*4"
+    Just (Add (Lit 2) (Mul (Lit 3) (Lit 4)))
+
+    *Calc> parseExp Lit Add Mul "2+3*"
+    Nothing
+
+ Leverage the assets of the UI team to implement the value-added function
+    evalStr :: String -> Maybe Integer
+
+ which evaluates arithmetic expressions given as a String, producing Nothing for
+ inputs which are not well-formed expressions, and Just n for well-formed inputs
+ that evaluate to n
+-}
+evalStr :: String -> Maybe Integer
+evalStr ex = case parseExp Lit Add Mul ex of
+               Just x -> Just $ eval x
+               Nothing -> Nothing
+
+-- Perhaps the above can be rewritten using guards
+evalStr' :: String -> Maybe Integer
+evalStr' ex
+  | isNothing x = Nothing
+  | otherwise   = Just $ (eval . fromJust) x
+  where x = parseExp Lit Add Mul ex
+
+
+-- I think there can be another implementation that uses function composition
+evalStr'' :: String -> Maybe Integer
+evalStr'' = fmap eval . parseExp Lit Add Mul
