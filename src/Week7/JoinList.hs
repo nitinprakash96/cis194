ca074fc47462e81134b067ff1466e776765e427a
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-14 07:15:18 +0530

    First 2 exercises

--- src/Week7/JoinList.hs
+++ src/Week7/JoinList.hs
@@ -6,6 +6,33 @@ import Week7.Editor
 import Week7.Sized
 
 data JoinList m a = Empty
-                  | Single a
+                  | Single m a
                   | Append m (JoinList m a) (JoinList m a)
   deriving (Show, Eq)
+
+
+---- Exercise 1 ----
+tag :: Monoid m => JoinList m a -> m
+tag Empty          = mempty
+tag (Single m _)   = m
+tag (Append m _ _) = m
+
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) a b = Append (tag a <> tag b) a b
+
+
+---- Exercise 2 ----
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ i j
+  | i >= 0 =
+    case j of
+      Empty      -> Nothing
+      Single _ a -> Just a
+      Append _ j1 j2
+        | size' > i -> indexJ i j1
+        | otherwise -> indexJ (i - size') j2
+        where size' = getSize $ size $ tag j1
+  | otherwise = Nothing
+
+
