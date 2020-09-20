abd6460abd74fbcd10aed453ed9560e53d4fb22f
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-21 01:48:47 +0530

    Solutions to week 10

---
+++ src/Week10/AParser.hs
@@ -0,0 +1,38 @@
+module Week10.AParser where
+
+import Control.Applicative
+import Data.Char
+
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+  where
+    f [] = Nothing -- fail on the empty input
+    f (x:xs) -- check if x satisfies the predicate
+             -- if so, return x along with the remainder
+             -- of the input (that is, xs)
+      | p x = Just (x, xs)
+      | otherwise = Nothing -- otherwise, fail
+
+
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+
+posInt :: Parser Integer
+posInt = Parser f
+  where
+    f xs
+      | null ns = Nothing
+      | otherwise = Just (read ns, rest)
+      where (ns, rest) = span isDigit xs
+
+
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (a, b) = (f a, b)
+
+
+instance Functor Parser where
+  fmap g (Parser f) = Parser $ fmap (first g) . f
