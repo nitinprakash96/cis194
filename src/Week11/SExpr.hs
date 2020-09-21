5159a1da7a11fc8f3e5dfe61bba37372db2abf46
Nitin Prakash <prakash.nitin63@gmail.com>
2020-09-21 16:29:44 +0530

    Solutions to week 11

---
+++ src/Week11/SExpr.hs
@@ -0,0 +1,55 @@
+{- CIS 194 HW 11
+   due Monday, 8 April
+-}
+
+module Week11.SExpr where
+
+import Week11.AParser
+import Control.Applicative
+import Data.Char
+
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+-- To parse one or more occurrences
+-- of p, run p once and then parse zero or
+-- more occurrences of p. To parse zero or
+-- more occurrences of p, try parsing one
+-- or more; if that fails, return the empty list.
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = (:) <$> p <*> zeroOrMore p
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+spaces :: Parser String
+spaces = zeroOrMore $ satisfy isSpace
+
+ident :: Parser String
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
+
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
+type Ident = String
+
+-- An "atom" is either an integer value or an identifier.
+data Atom = N Integer | I Ident
+  deriving Show
+
+-- An S-expression is either an atom, or a list of S-expressions.
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+
+
+parseSExpr :: Parser SExpr
+parseSExpr = undefined
